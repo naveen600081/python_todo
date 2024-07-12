e51530f14965b88d148d533198986b5476768d09
naveenkalaiselvam <naveen600081@gmail.com>
2024-07-12 10:52:58 -0500

    Create Basic GUI - No Interaction - day 16

--- guiDemo.py
+++ guiDemo.py
@@ -4,6 +4,6 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type todo ")
 addTodoTask = sg.InputText(tooltip="Enter a todo")
 add_button = sg.Button("Add")
-window = sg.Window("My Todo App", layout=[[label, addTodoTask, add_button]])
+window = sg.Window("My Todo App", layout=[[label], [addTodoTask, add_button]])
 window.read()
 window.close()
